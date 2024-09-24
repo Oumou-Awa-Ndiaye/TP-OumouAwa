839920ad25db0e0f63602b5515712352b6e2dfe0
noumouaw <oumouawa.ndiaye@ynovcom>
2024-09-24 15:39:53 +0200

    Main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func main() {
+// 	// Test de la fonction Ft_missing
+// 	fmt.Println(Ft_missing([]int{3, 1, 2}))                   // résultat : 0
+// 	fmt.Println(Ft_missing([]int{0, 1}))                      // résultat : 2
+// 	fmt.Println(Ft_missing([]int{9, 6, 4, 2, 3, 5, 7, 0, 1})) // résultat : 8
+// }
+
+func main() {
+	// Test de la fonction Ft_missing
+	fmt.Println(Ft_missing([]int{3, 1, 2}))                   // résultat : 0
+	fmt.Println(Ft_missing([]int{0, 1}))                      // résultat : 2
+	fmt.Println(Ft_missing([]int{9, 6, 4, 2, 3, 5, 7, 0, 1})) // résultat : 8
+}
