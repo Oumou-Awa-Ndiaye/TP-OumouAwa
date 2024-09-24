ead294a33d17a05d8f6620c1c77ffbc56e69a334
noumouaw <oumouawa.ndiaye@ynovcom>
2024-09-24 14:46:42 +0200

    Modif de la fonction Ft_Coin

--- Ft_Coin.go
+++ Ft_Coin.go
@@ -5,39 +5,34 @@ import (
 )
 
 func Ft_coin(coins []int, amount int) int {
-
-	result := solve(coins, amount, 0)
-
-	if result == 1000000 {
-		return -1
-	}
-	return result
-}
-
-//Fonction récursive pour trouver le nombre minimal de pièces
-
-func solve(coins []int, amount int, count int) int {
+	//D'abord ici il n'y aura pas de pièce si le montant est 0
 	if amount == 0 {
-		return count
+		return 0
 	}
 
-	if amount < 0 {
-		return 1000000
-	}
+	// Là j'ai inicialisé juste une valeur pour stocker le nombre minimal de pièces
+	minCoins := -1
 
-	minCoins := 1000000
+	// Là j'ai créée ma boucle pour parcourir toutes les pièces
 	for _, coin := range coins {
+		if coin <= amount {
+			// Je calcule combien de pièces de cette valeur sont nécessaires pour atteindre amount
+			temp := Ft_coin(coins, amount-coin)
+			if temp != -1 {
+				temp += 1
 
-		temp := solve(coins, amount-coin, count+1)
-		if temp < minCoins {
-			minCoins = temp
+				if minCoins == -1 || temp < minCoins {
+					minCoins = temp
+				}
+			}
 		}
 	}
+
 	return minCoins
 }
 
 func main() {
-	fmt.Println(Ft_coin([]int{1, 2, 5}, 11)) // resultat : 3
-	fmt.Println(Ft_coin([]int{2}, 3))        // resultat : -1
-	fmt.Println(Ft_coin([]int{1}, 0))        // resultat : 0
+	fmt.Println(Ft_coin([]int{1, 2, 5}, 11)) // résultat : 3
+	fmt.Println(Ft_coin([]int{2}, 3))        // résultat : -1
+	fmt.Println(Ft_coin([]int{1}, 0))        // résultat : 0
 }
